20dc01ca65dcc6a3640b3965368378edf47040ad
licho <lichobaron@gmail.com>
2021-04-04 12:42:23 -0500

    add cors

--- people-services/src/index.ts
+++ people-services/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import { PORT } from './config/express';
 import PersonController from './controllers/PersonController';
 
 const app = express();
 app.use( bodyParser.json() );
+app.use(cors())
 
 const personController = new PersonController( app );
 
